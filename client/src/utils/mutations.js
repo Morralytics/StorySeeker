382108f0db8e31d4c8d7d01dcc0532c8106bd9a7
Nick Morris <nicholasmorris2599@gmail.com>
2023-03-30 14:36:22 -0700

    Added all queries and mutations to the front-end by using the Apollo sandbox.

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,17 +1,54 @@
 import { gql } from '@apollo/client';
 
 export const LOGIN_USER = gql `
-
+mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
 `;
 
-export const ADD_USER = gql `
-
+export const CREATE_USER = gql `
+mutation createUser($username: String!, $email: String!, $password: String!) {
+    createUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
 `;
 
 export const SAVE_BOOK = gql `
-
+mutation saveBook($userId: ID!, $bookData: BookInput!) {
+    saveBook(userId: $userId, bookData: $bookData) {
+      _id
+      username
+      savedBooks {
+        bookId
+        title
+        description
+        authors
+        image
+        link
+      }
+    }
+  }
 `;
 
 export const DELETE_BOOK = gql `
-
+mutation deleteBook($bookId: ID!) {
+    deleteBook(bookId: $bookId) {
+      _id
+      username
+      savedBooks {
+        bookId
+      }
+    }
+  }
 `;
